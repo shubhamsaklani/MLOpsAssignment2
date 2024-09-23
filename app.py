08f11178aa461954fb499df663d6fcdeb486b842
shubhamsaklani <54273868+shubhamsaklani@users.noreply.github.com>
2024-09-23 21:30:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 import pandas as pd
 import streamlit as st
-from pycaret.regression import load_model, predict_model
+from pycaret.regression import predict_model, load_model
+import requests
+import os
 
-# Load trained Pipeline
-model = load_model("best_model")
+# Function to download the model file from Hugging Face
+def download_model(url, save_path):
+    response = requests.get(url)
+    with open(save_path, 'wb') as f:
+        f.write(response.content)
 
 # Streamlit app title
 st.title("Insurance Prediction App")
+
+# Define the model file path
+model_file_path = "best_model.pkl"
+
+# Check if the model file already exists, if not, download it
+if not os.path.exists(model_file_path):
+    with st.spinner("Downloading model..."):
+        download_model("https://huggingface.co/Suerz/MLOpsAssignment/resolve/main/best_model_v1.pkl", model_file_path)
+        st.success("Model downloaded!")
+
+# Load the downloaded model using PyCaret
+model = load_model('best_model')
 
 # Input fields for user data
 age = st.number_input("Age", min_value=0, value=36)
@@ -30,4 +47,3 @@ input_data = pd.DataFrame({
 if st.button("Predict"):
     predictions = predict_model(model, data=input_data)
     st.success(f"Prediction: {predictions['prediction_label'].iloc[0]}")
-
